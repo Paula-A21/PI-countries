f585046fe593a3be73af48e5713b618d093c4651
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-10-02 19:30:26 -0300

    arreglando bug del buscador

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,37 +1,31 @@
 import { searchCountry, getCountries } from "../../redux/actions";
 import { useDispatch } from 'react-redux';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import style from "./SearchBar.module.css";
 
-const SearchBar = () => {
+const SearchBar = ({ searchValue, onSearchChange }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    const [searchName, setSearchName] = useState('');
-    
-    useEffect(() => {
-        if (searchName.length) { //este use effect logra que como está escuchando 
-          dispatch(searchCountry(searchName));//los cambios que hay en el search
-        } else {                  //me vaya mostrando resultados a medida de que busco un país
-          dispatch(getCountries()); 
-        }
-      }, [searchName]); 
-    
-    const handleInputChange = (event) => { 
-        setSearchName(event.target.value);
-    };
-    
-    return (
-      <div className={style.SearchBar}>
-        <input
-          className={style.inputSearch}
-          type="search"
-          value={searchName} 
-          onChange={handleInputChange}
-          name="search-name"
-          placeholder="Insert a country name..."
-        />
-      </div>
-    );
+  useEffect(() => {
+      if (searchValue.length) {
+          dispatch(searchCountry(searchValue));
+      } else {
+          dispatch(getCountries());
+      }
+  }, [searchValue]);
+
+  return (
+    <div className={style.SearchBar}>
+      <input
+        className={style.inputSearch}
+        type="search"
+        value={searchValue}
+        onChange={onSearchChange}
+        name="search-name"
+        placeholder="Insert a country name..."
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
--- client/src/views/NavBar/NavBar.jsx
+++ client/src/views/NavBar/NavBar.jsx
@@ -1,16 +1,27 @@
 import {Link} from 'react-router-dom';
 import React from "react";
 import style from "./Navbar.module.css";
+import { useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 const NavBar = () => {
+    const [searchName, setSearchName] = useState('');
+
+    const handleInputChange = (event) => { 
+        setSearchName(event.target.value);
+    };
+
     return(
         <div className={style.NavBar}>
             <div>
-                <SearchBar className={style.SearchBar}/>
+                <SearchBar 
+                    className={style.SearchBar} 
+                    searchValue={searchName} 
+                    onSearchChange={handleInputChange} 
+                />
             </div>
             <div className={style.buttonsContainer}>
-                <Link to="/home">
+                <Link to="/home" onClick={() => setSearchName('')}>
                     <button>Home Page</button>
                 </Link>
                 <Link to={`/activities/form`}>
@@ -20,9 +31,13 @@ const NavBar = () => {
                     <button>Landing</button>
                 </Link>
             </div>
-
         </div>
     )
 };
 
 export default NavBar;
+
+
+
+
+
