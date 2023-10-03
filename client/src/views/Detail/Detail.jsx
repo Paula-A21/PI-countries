751e66e4c14850e40d8b178cc489ea95f14de0d5
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-10-02 21:08:02 -0300

    asd

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,31 +1,39 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { Fragment, useEffect, useState } from "react";
-import style from "./Detail.module.css"; // Importamos el módulo CSS específico para este componente
+import style from "./Detail.module.css";
 
 const Detail = () => {
   const { id } = useParams();
   const [country, setCountry] = useState({});
 
   useEffect(() => {
-    axios(`http://localhost:3001/countries/${id}`).then(({ data }) => {
-      if (data.id) {
-        setCountry(data);
-      } else {
-        window.alert("Can't find the detail of that country");
+    const fetchData = async () => {
+      try {
+        const response = await axios(`http://localhost:3001/countries/${id}`);
+        const data = response.data;
+        if (data.id) {
+          setCountry(data);
+        } else {
+          window.alert("Can't find the detail of that country");
+        }
+      } catch (error) {
+        console.error("Error fetching country details:", error);
       }
-    });
-    return setCountry({});
+    };
+
+    fetchData();
+
+    return () => setCountry({});
   }, [id]);
 
   return (
-    <div className={style.detailContainer}>
-      <h2 className={style.detailHeading}>Country Details</h2>
-      <img
-        src={country.flags}
-        alt={country.name}
-        className={style.detailImage}
-      />
+    <div
+      className={style.detailContainer}
+      style={{ backgroundImage: `url(${country.flags})` }}
+    >
+      {/* <h2 className={style.detailHeading}>Country Details</h2>
+      <img src={country.flags} alt={country.name} className={style.detailImage} /> */}
       <div className={style.detailItem}>
         <span className={style.detailLabel}>ID:</span>
         <span className={style.detailValue}>{id}</span>
@@ -54,19 +62,31 @@ const Detail = () => {
         <span className={style.detailLabel}>Population:</span>
         <span className={style.detailValue}>{country.population}</span>
       </div>
-      <div>
-        <span>Activities: </span>
+      <div className={style.detailItem}>
+        <span className={style.detailLabel}>Activities: </span>
         {country.Activities?.length > 0 ? (
           country.Activities.map((activity) => (
-            <div key={activity.name}>
-              <span>Name: {activity.name}</span>
-              <span>Dificulty: {activity.difficulty}</span>
-              <span>Season: {activity.season}</span>
-              <span>Duration: {activity.duration}</span>
+            <div key={activity.name} className={style.activity}>
+              <div className={style.activityColumn}>
+                <span className={style.detailLabel}>Name:</span>
+                <span className={style.detailValue}>{activity.name}</span>
+              </div>
+              <div className={style.activityColumn}>
+                <span className={style.detailLabel}>Difficulty:</span>
+                <span className={style.detailValue}>{activity.difficulty}</span>
+              </div>
+              <div className={style.activityColumn}>
+                <span className={style.detailLabel}>Season:</span>
+                <span className={style.detailValue}>{activity.season}</span>
+              </div>
+              <div className={style.activityColumn}>
+                <span className={style.detailLabel}>Duration:</span>
+                <span className={style.detailValue}>{activity.duration}</span>
+              </div>
             </div>
           ))
         ) : (
-          <span>There are not activities related with this country</span>
+          <span>There are no activities related to this country</span>
         )}
       </div>
     </div>
